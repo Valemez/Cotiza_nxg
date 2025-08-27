bcbc655a497dcfb6c3c07f7070065fc525f183b4
SisDev <sisdev79@gmail.com>
2025-08-27 12:50:44 -0600

    backend api inicio

--- backend/api.php
+++ backend/api.php
@@ -1,7 +1,9 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+require_once('');
 
 
 print_r($_SERVER);
